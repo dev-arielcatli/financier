46065d0bbb117e010182166f7782d4f53c90b1f3
Ariel Catli <dev.arielcatli@gmail.com>
2024-10-01 22:27:04 +0800

    Refactor entities_manager.py to include range_key_condition parameter in the load function

--- financier/shared/services/entities_manager.py
+++ financier/shared/services/entities_manager.py
@@ -1,9 +1,10 @@
 from typing import Type
 from shared.models.base import ItemModel
 
-def load(model_type: Type[ItemModel], item_id: any, filter_condition: any = None):
+def load(model_type: Type[ItemModel], item_id: any, range_key_condition: any = None, filter_condition: any = None):
     items = model_type.query(
         hash_key=item_id,
+        range_key_condition=range_key_condition,
         filter_condition=filter_condition
     ) 
     return list(items)
