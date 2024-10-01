7dbbb48af20fb402e4a07ebec3c6ddf5598df728
Ariel Catli <dev.arielcatli@gmail.com>
2024-10-01 22:37:21 +0800

    Refactor load_expense function in get.py to use load_item and handle DoesNotExist exception

--- financier/stacks/functions/handlers/expense/get.py
+++ financier/stacks/functions/handlers/expense/get.py
@@ -7,13 +7,16 @@ from shared.models.urls import EXPENSE_ID
 from shared.utils.requests import extract_path_parameters
 from shared.utils.responses import make_error_response, make_response
 
-from shared.services.entities_manager import load as load_expense
+from shared.services.entities_manager import load_item as load_expense
+
+from pynamodb.exceptions import DoesNotExist
 
 
 def handler(event, context):
     expense_id = extract_path_parameters(event)[EXPENSE_ID]
-    items_list = load_expense(model_type=ExpenseModel, item_id=DEFAULT_SYSTEM_ID, range_key_condition=ExpenseModel.item_id == expense_id)
-    if not items_list:
-        return make_error_response(ErrorCodes.MISSING_EXPENSE, HTTPStatus.NOT_FOUND)
+    try:
+        item = load_expense(model_type=ExpenseModel, item_id=expense_id)
+    except DoesNotExist:
+        return make_error_response(error_code=ErrorCodes.MISSING_EXPENSE, status=HTTPStatus.NOT_FOUND)
 
-    return make_response(items_list[0].attribute_values)
+    return make_response(item.attribute_values)
