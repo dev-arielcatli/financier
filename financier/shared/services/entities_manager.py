e11f81d09d65cfe5e5900ac951d30cf3a0c57d51
Ariel Catli <dev.arielcatli@gmail.com>
2024-10-01 22:37:11 +0800

    Refactor entities_manager.py to simplify the load function

--- financier/shared/services/entities_manager.py
+++ financier/shared/services/entities_manager.py
@@ -1,10 +1,7 @@
 from typing import Type
 from shared.models.base import ItemModel
 
-def load(model_type: Type[ItemModel], item_id: any, range_key_condition: any = None, filter_condition: any = None):
-    items = model_type.query(
-        hash_key=item_id,
-        range_key_condition=range_key_condition,
-        filter_condition=filter_condition
-    ) 
-    return list(items)
+from shared.config import DEFAULT_SYSTEM_ID
+
+def load_item(model_type: Type[ItemModel], item_id: any, hash_key: str = DEFAULT_SYSTEM_ID):
+    return model_type.get(hash_key=hash_key, range_key=item_id)
