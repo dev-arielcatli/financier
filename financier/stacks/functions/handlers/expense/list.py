e1a4582c2e1f8db9cca5103908e4934d74c5a8f1
Ariel Catli <dev.arielcatli@gmail.com>
2024-10-01 17:03:12 +0800

    fix issue with non-serializeable list items

--- financier/stacks/functions/handlers/expense/list.py
+++ financier/stacks/functions/handlers/expense/list.py
@@ -1,8 +1,19 @@
-import json
+from shared.models.expense import ExpenseModel
+from shared.utils.requests import extract_query_parameters
+
+from shared.models.urls import QUERY_PARAM_LAST_EVALUATED_KEY
+
+from shared.utils.responses import make_list_response
 
 
 def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"message": "Get expense"}),
-    }
+    last_evaluated = extract_query_parameters(event).get(QUERY_PARAM_LAST_EVALUATED_KEY, None)
+    items = ExpenseModel.query(
+        hash_key=ExpenseModel.user_id,
+        limit=10,
+        scan_index_forward=True,
+        last_evaluated_key=last_evaluated,
+    )
+
+    return make_list_response(items=[item.attribute_values for item in list(items)], last_evaluated_key=items.last_evaluated_key)
+    
