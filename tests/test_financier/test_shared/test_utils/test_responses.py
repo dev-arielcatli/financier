411fddfa0e9cdcbebd095242c9f63880937d54f8
Ariel Catli <dev.arielcatli@gmail.com>
2024-10-02 21:59:14 +0800

    add response service tests

---
+++ tests/test_financier/test_shared/test_utils/test_responses.py
@@ -0,0 +1,54 @@
+from shared.utils import responses
+from unittest import TestCase
+
+from http import HTTPStatus
+
+class TestResponses(TestCase):
+    def test_make_response(self):
+        self.assertEqual(responses.make_response({'test': 'test'}, HTTPStatus.OK), {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'isBase64Encoded': False,
+            'multiValueHeaders': {},
+            'body': '{"test": "test"}'
+        })
+
+    def test_make_response_no_content(self):
+        self.assertEqual(responses.make_response({}, HTTPStatus.NO_CONTENT), {
+            'statusCode': 204,
+            'headers': {'Content-Type': 'application/json'},
+            'isBase64Encoded': False,
+            'multiValueHeaders': {},
+            'body': None
+        })
+
+    def test_make_error_response(self):
+        self.assertEqual(responses.make_error_response(), {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'x-amzn-ErrorType': 500,
+            },
+            'isBase64Encoded': False,
+            'body': '{"error": "unhandled_error", "message": "Server got itself in trouble"}'
+        })
+
+    def test_make_error_response_custom(self):
+        self.assertEqual(responses.make_error_response(body='test'), {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'x-amzn-ErrorType': 500,
+            },
+            'isBase64Encoded': False,
+            'body': '{"error": "unhandled_error", "message": "test"}'
+        })
+
+    def test_make_list_response(self):
+        self.assertEqual(responses.make_list_response([1, 2, 3], 'test', HTTPStatus.OK), {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'isBase64Encoded': False,
+            'multiValueHeaders': {},
+            'body': '{"items": [1, 2, 3], "count": 3, "last_evaluated_key": "test"}'
+        })
