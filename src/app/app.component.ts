99d6b972b82933d70403ae96952fef3fa96d9bb8
Ariel Catli <dev.arielcatli@gmail.com>
2024-12-03 16:46:36 +0800

    [chore] prepare side nav and main components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'fn-root',
-  imports: [RouterOutlet],
+  imports: [MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'financier';
+
+  isSideNavOpen = signal<boolean>(false);
+
+  toggleSideNav() {
+    this.isSideNavOpen.update((currentStatus) => !currentStatus);
+  }
 }
