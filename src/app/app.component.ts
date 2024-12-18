d6c545d078ab3442ba349160be953ca5409d6a8e
Ariel Catli <dev.arielcatli@gmail.com>
2024-12-19 07:26:34 +0800

    chore: add routing for expense, income, budget, goal, and AI components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'fn-root',
-  imports: [MatSidenavModule],
+  imports: [RouterOutlet, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ExpenseComponent } from './pages/expense/expense.component';
+import { IncomeComponent } from './pages/income/income.component';
+import { BudgetComponent } from './pages/budget/budget.component';
+import { GoalComponent } from './pages/goal/goal.component';
+import { AiComponent } from './pages/ai/ai.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'expense',
+    component: ExpenseComponent,
+  },
+  { path: 'income', component: IncomeComponent },
+  { path: 'budget', component: BudgetComponent },
+  { path: 'goal', component: GoalComponent },
+  { path: 'ai', component: AiComponent },
+  { path: '', redirectTo: '/expense', pathMatch: 'full' },
+];
