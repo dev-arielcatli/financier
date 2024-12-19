918c76b92e02956e2cbcdd0d3cfc23bb479ae89e
Ariel Catli <dev.arielcatli@gmail.com>
2024-12-19 08:56:44 +0800

    feat(expense): add expense table with pagination and mock data

--- src/app/pages/expense/expense.component.ts
+++ src/app/pages/expense/expense.component.ts
@@ -1,28 +1,47 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { Expense } from './expense.model';
+import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
 import { MatInputModule } from '@angular/material/input';
-import {
-  MatDialog,
-  MatDialogModule,
-  MatDialogRef,
-} from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ExpenseFormComponent } from './expense-form/expense-form.component';
+import { Expense } from './expense.model';
+import { mockExpenses } from './mocks/expenses.mock';
+import { CommonModule } from '@angular/common';
+
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'fn-expense',
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatDialogModule,
+    MatTableModule,
+    MatPaginatorModule,
   ],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.scss',
 })
-export class ExpenseComponent {
+export class ExpenseComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) expenseTablePaginator!: MatPaginator;
+
   expenseDialog = inject(MatDialog);
+  expenseColumns: string[] = [
+    'name',
+    'description',
+    'quantity',
+    'amount',
+    'date',
+  ];
+  expenseDataSource = new MatTableDataSource<Expense>(mockExpenses);
+
+  ngAfterViewInit(): void {
+    this.expenseDataSource.paginator = this.expenseTablePaginator;
+  }
 
   onAddExpense(): void {
     console.log('Add Expense');
