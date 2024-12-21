927ca7b22733a7e62dea2d5c4c458d1a20dd239f
Ariel Catli <dev.arielcatli@gmail.com>
2024-12-21 19:56:21 +0800

    feat(expense): add sorting functionality to expense table

--- src/app/pages/expense/expense.component.ts
+++ src/app/pages/expense/expense.component.ts
@@ -23,6 +23,7 @@ import { CommonModule } from '@angular/common';
 
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { ShortenPipe } from '../../shared/shortener.pipe';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'fn-expense',
