1cb02f911bd3a67f30cec177aa19853c7fa4156a
Ariel Catli <dev.arielcatli@gmail.com>
2024-12-22 14:21:41 +0800

    feat(layout): replace sidenav placeholder with SidenavComponent and update layout structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,24 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from './pages/sidenav/sidenav.component';
 
 @Component({
   selector: 'fn-root',
-  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, MatIconModule],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    SidenavComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'financier';
 
-  isSideNavOpen = signal<boolean>(false);
+  isSideNavOpen = signal<boolean>(true);
 
   toggleSideNav() {
     this.isSideNavOpen.update((currentStatus) => !currentStatus);
